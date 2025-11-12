530e297bbd4842c4b54fbf9cb3ac85346a8b95e5
Shalom2003 <130524968+Shalom2003@users.noreply.github.com>
2025-11-12 18:22:47 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+    /* @type {import('tailwindcss').Config} */
+    module.exports = {
+      content: [
+        // ... your content paths
+      ],
+      theme: {
+        extend: {
+          fontFamily: {
+            righteous: ['Righteous'], // 'sans-serif' as a fallback
+          },
+        },
+      },
+      plugins: [],
+    };
